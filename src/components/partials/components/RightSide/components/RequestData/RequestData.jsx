d92ebd2072958b4e8f170857bded43887325d0df
Codebug <prasanna2904>
2022-04-04 23:46:01 +0530

    added params in table

--- src/components/partials/components/RightSide/components/RequestData/RequestData.jsx
+++ src/components/partials/components/RightSide/components/RequestData/RequestData.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ParamsData from './components/ParamsData/ParamsData'
 
 const RequestData = ({calledApi}) => {
-    console.log(calledApi)
   return (
     <div>
         <ParamsData calledApi={calledApi} />
--- src/components/partials/components/RightSide/components/RequestData/components/ParamsData/ParamsData.jsx
+++ src/components/partials/components/RightSide/components/RequestData/components/ParamsData/ParamsData.jsx
@@ -1,9 +1,49 @@
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+  Input,
+  Checkbox
+} from '@chakra-ui/react'
+
 import {getParams} from '../../../../../../../../services/auth/helpers'
 const ParamsData = ({calledApi}) => {
     const params = calledApi[0]?.text ? getParams(calledApi[0]?.text) : ""
-    console.log(params)
     return (
-    <div>{calledApi[0]?.text}</div>
+    <div>
+      <TableContainer>
+        <Table variant='simple'>
+          <Thead>
+            <Tr>
+              <Th></Th>
+              <Th>Key</Th>
+              <Th>Value</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+          {
+            Object.entries(params).map(([key, value]) => {
+              return(
+                <Tr key={key}>
+                  <Td><Checkbox></Checkbox></Td>
+                  <Td>
+                    <Input placeholder='small size' size='sm' value={key} />
+                  </Td>
+                  <Td>
+                    <Input placeholder='small size' size='sm' value={value} />
+                  </Td>
+                </Tr>
+              )  
+            })
+          }
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </div>
   )
 }
 
